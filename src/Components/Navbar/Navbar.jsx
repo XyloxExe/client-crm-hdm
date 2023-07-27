da22d2915300c7adf3a909cd26de2b5caf98fe00
aboalsim114 <aboalsim114@gmail.com>
2023-07-27 12:45:40 +0200

    mise a jour

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { slide as Menu } from 'react-burger-menu';
+import AvatarUser from "../images/avatar7.png";
+import avatarBorder from "../images/avatarBorder.png"
+import { BiSolidDashboard } from 'react-icons/bi';
+import { FaUserAlt } from 'react-icons/fa';
 
+
+import axios from "axios";
 export default function NavBar() {
 
     const token = localStorage.getItem("authToken")
     const userId = localStorage.getItem("userId")
+    const [userdata,setUserData] = useState([])
    
+
+    useEffect(() => {
+      const fetchUserData = async () => {
+        try {
+          const url = `https://localhost:8000/api/user/${userId}`;
+          const response = await axios.get(url);
+          setUserData(response.data);
+        } catch (error) {
+          console.error("Erreur lors de la récupération des données utilisateur :", error);
+        }
+      };
+  
+      fetchUserData();
+    }, [userId]);
 
   
     return(
@@ -28,7 +49,20 @@ export default function NavBar() {
                  : 
 
                  <>
-                 <li><span><i className="fa fa-dashboard"></i></span><Link style={{color : "#fff"}} to={`/profile/${userId}`}><i className="fa fa-dashboard"></i>Mon Porfile</Link></li>
+                 <div className="userinfoNavbar">
+                  <div className="borderImgUserNav">
+                    <div className="borderAvataruser">
+                    <img src={avatarBorder} alt="" srcset="" />
+                  <img src={AvatarUser} className="AvatarUser" alt="" srcset="" />
+                    </div>
+                  </div>
+                  <p>{userdata.firstName} {userdata.lastName}</p>
+                 </div>
+                 <div className="DashboardNavLink">
+
+                 <li><BiSolidDashboard className="navbarIcon"/> <Link  to={`/profile/${userId}`}><i ></i>Dashboard</Link></li>
+                 </div>
+                 <li> <span><i className="fa fa-dashboard"></i></span><Link style={{color : "#fff"}} to={`/profile/${userId}`}><i className="fa fa-dashboard"></i>Mon Porfile</Link></li>
                  <li><span><i className="fa fa-dashboard"></i></span><Link style={{color : "#fff"}} to="/logout"><i className="fa fa-dashboard"></i>deconnexion</Link></li>
                  </>
 
